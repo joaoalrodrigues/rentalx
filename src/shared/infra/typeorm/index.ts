364466cca9894ebd4d9dc926f9a362db2646109b
joaoalrodrigues <48808876+joaoalrodrigues@users.noreply.github.com>
2021-06-23 14:49:44 +0200

    docker config update

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,11 +1,10 @@
 import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-export default async (host = "database"): Promise<Connection> => {
+export default async (): Promise<Connection> => {
     const defaultOptions = await getConnectionOptions();
 
     return createConnection(
         Object.assign(defaultOptions, {
-            host: isTestEnvironment() ? "localhost" : host,
             database:
                 isTestEnvironment()
                     ? "rentalx_test"
