cc22303490887769419e33453be0f4a813699b55
joaoalrodrigues <48808876+joaoalrodrigues@users.noreply.github.com>
2021-06-24 12:59:38 +0200

    added sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -3,6 +3,8 @@ import "dotenv/config";
 import cors from "cors";
 import express from "express";
 import "express-async-errors";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 import swaggetUi from "swagger-ui-express";
 
 import rateLimiter from "@shared/infra/http/middlewares/rateLimiter";
@@ -19,6 +21,18 @@ const app = express();
 
 app.use(rateLimiter);
 
+Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+        new Sentry.Integrations.Http({ tracing: true }),
+        new Tracing.Integrations.Express({ app }),
+    ],
+    tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(express.json());
 
 app.use("/api-docs", swaggetUi.serve, swaggetUi.setup(swaggerFile));
@@ -28,6 +42,8 @@ app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
 app.use(cors());
 app.use(routes);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use(handleErrors);
 
