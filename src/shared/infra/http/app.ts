292697d823d435530217b7c8c84b31c057bf27ef
joaoalrodrigues <48808876+joaoalrodrigues@users.noreply.github.com>
2021-06-24 10:48:22 +0200

    added cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 import "dotenv/config";
+import cors from "cors";
 import express from "express";
 import "express-async-errors";
 import swaggetUi from "swagger-ui-express";
@@ -22,6 +23,7 @@ app.use("/api-docs", swaggetUi.serve, swaggetUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
+app.use(cors());
 app.use(routes);
 
 app.use(handleErrors);
